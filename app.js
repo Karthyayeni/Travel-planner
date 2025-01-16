0b0fa855df8cbcf57fc562b4f55e181b2730baa1
Karthyayeni <168815126+Karthyayeni@users.noreply.github.com>
2025-01-17 00:02:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors());
 // Route for login
 app.post("/", async (req, res) => {
     const { email, password } = req.body;
-
+    
     if (!email || !password) {
         return res.status(400).json({ error: "Email and password are required." });
     }
